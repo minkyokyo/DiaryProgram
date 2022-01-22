33bcad9693a33d7bf57f2ed2d6d3482a38945074
kty4119 <kty4119@naver.com>
2022-01-22 20:24:23 +0900

    new code

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import sys
 from PyQt5.QtWidgets import *
+#from PyQt5 import uic
 
 app = QApplication(sys.argv)
+
+#form_class = uic.loadUiType("MainWindow.ui")[0]
+#print(form_class)
 
 window = QWidget()
 window.show()
 
 app.exec_()
+'''
+class WindowClass(QMainWindow, form_class):
+    def __init(self):
+        super.setupUi(self)
+        self.pushButton.clicked.connect(self.functionStart)
+    
+    def functionStart(self):
+        f = open('일기장이름.txt','w')
+        f.close()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = QWidget()
+    Window = WindowClass()
+    Window.show()
+    app.exec_()
+'''
