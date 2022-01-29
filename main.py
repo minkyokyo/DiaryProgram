e8182aff7371fc711afbd6eead16e40dcffb85dd
mango <minkyokyo0807@gmail.com>
2022-01-29 17:12:37 +0900

    py파일을 연결, 일기내용 파일에 저장

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import sys
 from PyQt5.QtWidgets import *
-#from PyQt5 import uic
+from PyQt5 import uic
 
-app = QApplication(sys.argv)
 
-#form_class = uic.loadUiType("MainWindow.ui")[0]
-#print(form_class)
+#MainWindow.ui파일을 .py로 변환한 것을 불러온다.
+from MainWindow import Ui_MainWindow
 
-window = QWidget()
-window.show()
-
-app.exec_()
-'''
-class WindowClass(QMainWindow, form_class):
-    def __init(self):
-        super.setupUi(self)
-        self.pushButton.clicked.connect(self.functionStart)
+class WindowClass(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        #UI 선언
+        self.main_ui =Ui_MainWindow() #Ui_MainWindow()클래스가 메인 윈도우.
+        self.main_ui.setupUi(self)
+        self.main_ui.pushButton.clicked.connect(self.functionStart)
+        self.show()
+        #self.pushButton.clicked.connect(self.functionStart)
     
     def functionStart(self):
         f = open('일기장이름.txt','w')
+        diary_content=self.main_ui.plainTextEdit.toPlainText()
+        f.write(diary_content)
         f.close()
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = QWidget()
     Window = WindowClass()
-    Window.show()
     app.exec_()
-'''
